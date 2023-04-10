0298f78ce394693ca4df4496cfd762408398906d
HeeSoon <phs970824@daum.net>
2023-04-11 02:46:48 +0900

    fetch로 json 데이터 가져오기 성공. json파일을 src폴더 내에 두어서 계속 오류가 났음 -> public 폴더로 이동하니 정상 작동

--- src/modules/Main.js
+++ src/modules/Main.js
@@ -1,5 +1,25 @@
 import React from "react";
 
 export const Main = () => {
-  return <div id="main"></div>;
+  async function Data() {
+    const response = await fetch("../product.json");
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+  }
+  Data();
+
+  return (
+    <div id="main">
+      <div className="content">
+        <div className="main-prod">
+          메인슈즈 3개 슬라이드, 컬레션보다 느리게
+        </div>
+        <div className="collection">
+          슈즈 컬렉션 10개, 5개 보이고 반복 슬라이드로
+        </div>
+        <div className="event">이벤트 배너</div>
+      </div>
+    </div>
+  );
 };
