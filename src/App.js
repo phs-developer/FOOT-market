6cb90efb99f9409f72ccfd7cf1cab84902f0209e
HeeSoon <phs970824@daum.net>
2023-04-11 01:16:14 +0900

    FOOT 신발 마켓 구현 시작. header, footer 구현

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Header } from "./modules/Header.js";
+import { Main } from "./modules/Main.js";
+import { Footer } from "./modules/Footer.js";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/modules/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCartShopping,
+  faUser,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
+
+export const Header = () => {
+  return (
+    <header>
+      <div id="header">
+        <h1 className="logo">
+          <img src="logo.png" alt="로고이미지" />
+        </h1>
+        <form className="search-bar">
+          <input type="text" placeholder="키워드를 입력하세요!" />
+          <button type="submit">
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </form>
+        <div className="user-info">
+          <FontAwesomeIcon className="cart" icon={faCartShopping} role="cart" />
+          <FontAwesomeIcon className="user" icon={faUser} role="user" />
+        </div>
+      </div>
+      <div className="nav">
+        <ul className="nav-container">
+          <li className="nav-item">
+            <a href="#!">BRAND</a>
+          </li>
+          <li className="nav-item">
+            <a href="#!">MAN</a>
+          </li>
+          <li className="nav-item">
+            <a href="#!">WOMAN</a>
+          </li>
+          <li className="nav-item">
+            <a href="#!">KIDS</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
