7e3e064182ebeee02e9063a8e9b550aafbae95f1
HeeSoon <phs970824@daum.net>
2023-04-11 17:09:28 +0900

    MainProd --> 중앙 큰 이미지 구현.

---
+++ src/modules/MainProd.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
+export const MainProd = ({ props }) => {
+  // 메인슈즈 3개 슬라이드, 컬레션보다 느리게
+  if (!Array.isArray(props)) return;
+
+  return (
+    <div className="main-prod">
+      <button className="prev btn">
+        <FontAwesomeIcon icon={faAngleLeft} />
+      </button>
+      <div className="prod-wrap">
+        <div className="prod-box">
+          <img className="prod-item" src={props[7].img} alt={props[7].img} />
+        </div>
+        <div className="prod-box">
+          <img className="prod-item" src={props[5].img} alt={props[5].img} />
+        </div>
+        <div className="prod-box">
+          <img className="prod-item" src={props[0].img} alt={props[0].img} />
+        </div>
+      </div>
+      <button className="next btn">
+        <FontAwesomeIcon icon={faAngleRight} />
+      </button>
+    </div>
+  );
+};
