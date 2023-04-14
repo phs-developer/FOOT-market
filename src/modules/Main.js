f699d14e307708f2fbcd823c79a1846203915560
HeeSoon <phs970824@daum.net>
2023-04-15 01:10:17 +0900

    section title 추가, json 데이터 받기 --> app.js 에서 main.js로 이동

--- src/modules/Main.js
+++ src/modules/Main.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Section } from "./Section.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlane } from "@fortawesome/free-solid-svg-icons";
 
-export const Main = ({ data }) => {
+export const Main = () => {
+  //json 데이터 받기
+  const [data, setData] = useState("");
+  useEffect(() => {
+    async function getFetch() {
+      const res = await fetch("product.json");
+      const result = await res.json();
+      setData(result);
+    }
+    getFetch();
+  }, []);
+
   // MoveBtn --> 앵커, top버튼
   const MoveBtn = () => {
     const scrollToTop = () => {
@@ -40,6 +51,32 @@ export const Main = ({ data }) => {
     <div id="main">
       <MoveBtn />
       <Section name="running" data={data.running} />
+      <div className="brand-focus">
+        <div className="title">
+          <h3>Brand focus</h3>
+          <span>추천 브랜드</span>
+        </div>
+        <div className="brand">
+          <a href="!#">
+            <img src="image/Brand/nike.jpg" alt="나이키" />
+          </a>
+          <a href="!#">
+            <img src="image/Brand/new balance.jpg" alt="뉴발란스" />
+          </a>
+          <a href="!#">
+            <img src="image/Brand/jordan.jpg" alt="조던" />
+          </a>
+          <a href="!#">
+            <img src="image/Brand/adidas.jpg" alt="아디다스" />
+          </a>
+          <a href="!#">
+            <img src="image/Brand/crocs.jpg" alt="크록스" />
+          </a>
+          <a href="!#">
+            <img src="image/Brand/converse.jpg" alt="컨버스" />
+          </a>
+        </div>
+      </div>
       <Section name="slipper" data={data.slipper} />
       <Section name="shoes" data={data.shoes} />
       <Section name="other" data={data.other} />
--- src/modules/Section.js
+++ src/modules/Section.js
@@ -3,10 +3,13 @@ import { Event } from "./Event.js";
 import { ContentItem } from "./ContentItem.js";
 
 export const Section = ({ name, data }) => {
+  const sectionTitle = name.toUpperCase();
+
   return (
     <div id={name} className="section">
       <Event name={name} />
       <div className="content">
+        <h2 className="section-title">{sectionTitle}</h2>
         <ContentItem data={data} name={name} />
       </div>
     </div>
