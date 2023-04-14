71cc7111c5b7452e0c6199b18127e749c86cd020
HeeSoon <phs970824@daum.net>
2023-04-15 01:12:18 +0900

    main-prod 버튼 기능, 상품 자동 슬라이드 구현

--- src/modules/MainProd.js
+++ src/modules/MainProd.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronRight,
@@ -6,26 +6,55 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export const MainProd = ({ props }) => {
-  // 메인슈즈 3개 슬라이드, 컬레션보다 느리게
+  const animationHandler = useRef(null);
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      if (index < 3) {
+        setIndex(index + 1);
+        animationHandler.current.style.transition = "all 1s";
+      } else {
+        setIndex(0);
+        animationHandler.current.style.transition = "";
+      }
+    }, 2000);
+  }, [index]);
   if (!Array.isArray(props)) return;
+
+  //버튼 핸들러
+  const prevBtn = () => {
+    setIndex(index - 1);
+  };
+  const nextBtn = () => {
+    setIndex(index + 1);
+  };
+
+  function MakeMainProdItem({ props }) {
+    return (
+      <div className="prod-box">
+        <img className="prod-item" src={props.img} alt={props.img} />
+      </div>
+    );
+  }
 
   return (
     <div className="main-prod">
-      <button className="prev btn">
+      <button className="prev btn" onClick={prevBtn}>
         <FontAwesomeIcon icon={faChevronLeft} />
       </button>
-      <div className="prod-wrap">
-        <div className="prod-box">
-          <img className="prod-item" src={props[7].img} alt={props[7].img} />
-        </div>
-        <div className="prod-box">
-          <img className="prod-item" src={props[5].img} alt={props[5].img} />
-        </div>
-        <div className="prod-box">
-          <img className="prod-item" src={props[0].img} alt={props[0].img} />
+      <div className="prod-hidden">
+        <div
+          className="prod-wrap"
+          style={{ transform: `translateX(-${index * 500}px)` }}
+          ref={animationHandler}
+        >
+          <MakeMainProdItem props={props[7]} />
+          <MakeMainProdItem props={props[5]} />
+          <MakeMainProdItem props={props[0]} />
+          <MakeMainProdItem props={props[7]} />
         </div>
       </div>
-      <button className="next btn">
+      <button className="next btn" onClick={nextBtn}>
         <FontAwesomeIcon icon={faChevronRight} />
       </button>
     </div>
