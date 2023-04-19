8945c9044122676a74c3507daba0f7d1b35f198d
HeeSoon <phs970824@daum.net>
2023-04-19 19:10:42 +0900

    모바일 사이즈 main-prod 사이즈 축소

--- src/modules/main/MainProd.js
+++ src/modules/main/MainProd.js
@@ -3,22 +3,24 @@ import React, { useState, useEffect, useRef } from "react";
 export const MainProd = ({ props }) => {
   const animationHandler = useRef();
   const [index, setIndex] = useState(0);
+  const [prodWidth, setProdWidth] = useState(500);
 
-  // // 슬라이드 구현
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     if (animationHandler.current) {
-  //       //useRef() 오류처리
-  //       if (index < 3) {
-  //         setIndex(index + 1);
-  //         animationHandler.current.style.transition = "all 1s";
-  //       } else {
-  //         setIndex(0);
-  //         animationHandler.current.style.transition = "";
-  //       }
-  //     }
-  //   }, 2000);
-  // }, [index]);
+  // 슬라이드 구현
+  useEffect(() => {
+    setProdWidth(document.getElementsByClassName("main-wrap")[0].clientWidth);
+    setTimeout(() => {
+      if (animationHandler.current) {
+        //useRef() 오류처리
+        if (index < 3) {
+          setIndex(index + 1);
+          animationHandler.current.style.transition = "all 1s";
+        } else {
+          setIndex(0);
+          animationHandler.current.style.transition = "";
+        }
+      }
+    }, 2000);
+  }, [index]);
 
   if (!Array.isArray(props)) return;
 
@@ -42,7 +44,7 @@ export const MainProd = ({ props }) => {
         <div
           className="main-wrap"
           ref={animationHandler}
-          style={{ transform: `translateX(-${index * 400}px)` }}
+          style={{ transform: `translateX(-${index * prodWidth}px)` }}
         >
           <MakeMainProdItem />
         </div>
