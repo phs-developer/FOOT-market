3a131f940d8cdf93bc352b567569f8e673756927
HeeSoon <phs970824@daum.net>
2023-07-06 16:59:28 +0900

    장바구니 삭제 버튼 구현

--- src/modules/cart/cart.js
+++ src/modules/cart/cart.js
@@ -1,43 +1,69 @@
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { inCart } from "../store/recoil.js";
 import "./cart.css";
 
 export const Cart = ({ data }) => {
-  const cartProd = useRecoilValue(inCart);
+  const [prodId, setProdId] = useRecoilState(inCart);
 
-  //모든 아이템 중 장바구니 아이템만 뽑기
+  // prodId 상품의 정보 추출 -> prodInCart
   const prodKeys = Object.keys(data);
-  const allItem = [];
+  const prodInCart = [];
   prodKeys.forEach((e) => {
     for (let i = 0; i < data[e].length; i++) {
-      if (cartProd.includes(data[e][i].id)) {
-        allItem.push(data[e][i]);
+      if (prodId.includes(data[e][i].id)) {
+        prodInCart.push(data[e][i]);
       }
     }
   });
-  const total = allItem.reduce((acc, curr) => acc + Number(curr.price), 0);
+  const total = prodInCart.reduce((acc, curr) => acc + Number(curr.price), 0);
+
+  //삭제버튼
+  function handleDeleteInCart(e) {
+    setProdId(prodId.filter((prodId) => prodId !== e));
+    console.log(prodId[0]);
+  }
 
   return (
     <section className="cart">
-      <h2>장바구니</h2>
+      <h2>장바구니 ({prodInCart.length})</h2>
       <div>
-        <ul className="cart-list">
-          {allItem.map((e) => {
-            return (
-              <li key={e.id}>
-                <img className="prod-img" src={e.img} alt={e.img} />
-                <div>
-                  <p>{e.name}</p>
-                  <p>{Number(e.price).toLocaleString()}원</p>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="total">
-        <p>상품 갯수 : {allItem.length}</p>
-        <p>총 가격 : {total.toLocaleString()}원</p>
+        <table className="cart-list">
+          <thead>
+            <tr>
+              <th>이미지</th>
+              <th>상품명</th>
+              <th>판매가</th>
+              <th>삭제</th>
+            </tr>
+          </thead>
+          <tbody>
+            {prodInCart.map((e) => {
+              return (
+                <tr key={e.id} className="cart-item">
+                  <td>
+                    <img src={e.img} alt={e.img} />
+                  </td>
+                  <td>{e.name}</td>
+                  <td>{Number(e.price).toLocaleString()}원</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        handleDeleteInCart(e.id);
+                      }}
+                    >
+                      삭제
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+          <tfoot className="total">
+            <tr>
+              <td colSpan="4">총 금액 : {total.toLocaleString()}원</td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     </section>
   );
